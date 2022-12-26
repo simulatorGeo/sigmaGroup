e3a8455127c9cdea712a908b9b8e8b4433d1fda9
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-12-26 15:09:49 +0400

    clear

--- consult/en/js/main.js
+++ consult/en/js/main.js
@@ -1,7 +1,9 @@
 const mailButton = document.getElementById('mailButton')
 const formWrapper = document.getElementById('formWrapper')
+const closeBtn = document.getElementById('closeButton')
 const changeState = ()=>{
     console.log(formWrapper.hidden)
     formWrapper.hidden=!formWrapper.hidden
 }
 mailButton.addEventListener('click',changeState)
+closeBtn.addEventListener('click',changeState)
